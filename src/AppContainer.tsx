8674e8eef46b7b1cb1862859929fed22b573252a
joshua-augustinus <joshua.augustinus@gmail.com>
2020-10-31 23:26:58 +1100

    First commit

---
+++ src/AppContainer.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createAppContainer, SafeAreaView } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { getNavigationOptions } from './HeaderHelper';
+
+
+
+class HomeScreen extends React.Component {
+    render() {
+        return (
+            <SafeAreaView style={{flex:1}}>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>Home Screen</Text>
+                </View>
+            </SafeAreaView>
+
+        );
+    }
+}
+
+const RootStack = createStackNavigator({
+    Home: {
+        screen: HomeScreen,
+        navigationOptions: ({ navigation }) => {
+            return getNavigationOptions(navigation);
+        }
+    },
+});
+const AppContainer = createAppContainer(RootStack);
+
+// Now AppContainer is the main component for React to render
+export { AppContainer };
